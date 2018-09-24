2f062b831db78f5679ba17336accc35fcb9d61de
Florent Perticoz <perticoz@hotmail.com>
2018-09-24 17:29:21 +0200

    changement titre

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mewAngular';
+  title = 'Welcome to Florent Perticoz Website';
 }
